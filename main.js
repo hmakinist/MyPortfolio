f79c4fb110c39c9ecb7ad3e9709b7857f48998d2
harry <hmakinist@hotmail.com>
2021-07-15 01:08:32 +0100

    minor js changes

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ navButtons.forEach((navButton, i) => {
   });
 });
 
-const typeWriter = () => {// recursion
+const typeWriter = () => {
   if (count < text.length) {
     typing.textContent += text.charAt(count);
     count++;
@@ -25,36 +25,11 @@ window.onload = function () {
   }, 500);
 }
 
-
-
-// function isElementOutViewport(el){
-//   var rect = el.getBoundingClientRect();
-//   return rect.bottom < 0 || rect.right < 0 || rect.left > window.innerWidth || rect.top > window.innerHeight;
-// }
-
-// function isInView(el) {
-//   const box = el.getBoundingClientRect();
-//   return (box.top + 120) < window.innerHeight && box.bottom >= 200;
-// }
-// window.addEventListener('scroll', () =>{
-//   console.log(isInView(sections[1]));
-//   if (isInView(sections[1]) === true) {
-//     anchors[1].style.color = '#f8c471';
-//   } else {
-//     anchors[1].style.color = '#fff';
-//   }
-// });
-// https://stackoverflow.com/questions/123999/how-can-i-tell-if-a-dom-element-is-visible-in-the-current-viewport
-
-
-// hihlights the current nav button
 window.addEventListener('scroll', () => {
   let current = '';
-  // console.log(pageYOffset);
   sections.forEach(section => {
-    const sectionTop = section.offsetTop;// 60 is navbar height
+    const sectionTop = section.offsetTop;
     const sectionHeight = section.clientHeight;
-    //console.log(sectionTop);
     if (pageYOffset > (sectionTop - sectionTop / 5)) {
       current = section.getAttribute('id');
     } else if (pageYOffset < 10) {
